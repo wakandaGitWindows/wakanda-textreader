0b3f1d5c16a01e9ea84a7670d6bd569378ec56ef
wakandaGitWindows <wakanda.git.windows@gmail.com>
2015-08-11 17:09:46 +0200

    Update textreader.js
    getLastlines method returns the last "n" lines of a text file

--- textreader.js
+++ textreader.js
@@ -59,4 +59,48 @@ TextReader.prototype.charPosToBufferPos = function(position){
 	return bufferPos;
 };
 
+
+
+TextReader.prototype.getLastLines = function(lineNumbers){
+	
+				var unsortedLines=[];
+				var sortedLines=[];
+				var stream = TextStream(file, "read");  
+				var line = 1 ;  
+
+				while(stream.end( )==false)
+				{
+				   	 var currentLineContent = stream.read("") ; 				    
+					 if(currentLineContent=="") continue;				    
+					 else
+					 {     unsortedLines[line%lineNumbers]= { content:currentLineContent,
+					     					   lineNumber:line
+					     						};
+						line++;
+					}
+				}
+
+				for(var i = 0 ; i<lineNumbers-1 ; i++)
+				{			
+						if(unsortedLines[i].lineNumber<unsortedLines[i+1].lineNumber)
+						{							
+							for(j2=i ;  j2<lineNumbers ;j2++ )
+							{
+								sortedLines.push(unsortedLines[j2].content);
+							}
+							
+							for(var j =0 ; j <i ; j++)
+							{
+								sortedLines.push(unsortedLines[j].content);
+								
+							}							
+							break;
+							
+						}					
+					
+				}
+				
+				return sortedLines;
+};
+
 module.exports = TextReader;
